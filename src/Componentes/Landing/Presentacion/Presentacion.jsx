15cb9e9ad8585fb7508d65ecc32d7708136f320c
Maximiliano Fonseca <maximilianofonseca19@gmail.com>
2023-11-19 20:54:14 -0300

    cambios realizados

--- src/Componentes/Landing/Presentacion/Presentacion.jsx
+++ src/Componentes/Landing/Presentacion/Presentacion.jsx
@@ -16,11 +16,10 @@ function Presentacion() {
           <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
             <div class="text-center lg:w-2/3 w-full">
               <h1 class="flex aling-center justify-center title-font sm:text-8xl text-7xl mb-4 font-medium text-white">
-                Fast<h1 class="ml-3 mr-3 text-red-700">COLOR</h1>
+                Pintureria Automotriz Fast
+                <h1 class="ml-3 mr-3 text-red-700">COLOR</h1>
               </h1>
-              <h2 class="text-3xl text-red-500 tracking-widest font-medium title-font mb-1">
-                Pintureria Automotriz
-              </h2>
+              <h2 class="text-3xl text-red-500 tracking-widest font-medium title-font mb-1"></h2>
               <p class="mb-9 mt-9 text-xl leading-relaxed">
                 Somos tu destino confiable y experto en el mundo de la
                 automoción, dedicados a brindarte la más alta calidad en
